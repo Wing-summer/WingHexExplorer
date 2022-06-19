dfd1bb5eca06bb15774e67b5a59c02a6ef555670
wingsummer <1326224942@qq.com>
2022-06-19 23:15:55 +0800

    shenmo 修复 ubuntu下的显示问题

--- WingHexExplorer/main.cpp
+++ WingHexExplorer/main.cpp
@@ -20,21 +20,24 @@ DWIDGET_USE_NAMESPACE
 
 int main(int argc, char *argv[]) {
 
-  //解决 root/ubuntu 主题样式走形 
-
-    qputenv("XDG_CURRENT_DESKTOP", "Deepin");
-
+  //解决 root/ubuntu 主题样式走形
+  qputenv("XDG_CURRENT_DESKTOP", "Deepin");
   QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-    // 程序内强制添加"-platformtheme deepin"参数喂给Qt让Qt正确使用Deepin主题修复各种奇怪样式问题
-    QVector<char*> fakeArgs(argc + 2);
-    fakeArgs[0] = argv[0];
-    fakeArgs[1] = "-platformtheme";
-    fakeArgs[2] = "deepin";
-    for(int i = 1; i < argc; i++) fakeArgs[i + 2] = argv[i];
-    int fakeArgc = argc + 2; 
-    WingHexApplication a(fakeArgc, fakeArgs.data());
 
+  // 程序内强制添加 -platformtheme
+  // deepin 参数喂给 Qt 让 Qt 正确使用 Deepin 主题修复各种奇怪样式问题
+  QVector<char *> fakeArgs(argc + 2);
+  char fa1[] = "-platformtheme";
+  char fa2[] = "deepin";
+  fakeArgs[0] = argv[0];
+  fakeArgs[1] = fa1;
+  fakeArgs[2] = fa2;
 
+  for (int i = 1; i < argc; i++)
+    fakeArgs[i + 2] = argv[i];
+  int fakeArgc = argc + 2;
+
+  WingHexApplication a(fakeArgc, fakeArgs.data());
   QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
 
   auto s = a.applicationDirPath() + "/lang/default.qm";
