6675ea55da95e4080e2affc7340e0372f1cdf04e
wingsummer <1326224942@qq.com>
2022-08-02 08:50:15 +0800

    更新插件系统插件接口

--- WingHexExplorer/plugin/iwingplugin.h
+++ WingHexExplorer/plugin/iwingplugin.h
@@ -11,7 +11,7 @@
 #include <QWidget>
 #include <QtCore>
 
-#define SDKVERSION 5
+#define SDKVERSION 6
 #define GETPLUGINQM(name)                                                      \
   (QCoreApplication::applicationDirPath() + "/plglang/" + name)
 #define PLUGINDIR (QCoreApplication::applicationDirPath() + "/plugin")
